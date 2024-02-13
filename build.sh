79bc984b9b2f32c94eabe13983ae9492e8124d42
afii <muhammadafnanahmed07@gmail.com>
2024-02-13 23:32:01 +0500

    First Check

---
+++ build.sh
@@ -0,0 +1,7 @@
+# Build the project
+echo "Building the project..."
+python -m pip install -r requirements.txt
+
+echo "Make Migration..."
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
